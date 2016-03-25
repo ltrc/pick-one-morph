213e3cb880ebec5b287c4cc04f2e508fcf112f0a
Nehal J Wani <nehaljw.kkd1@gmail.com>
2016-03-25 19:44:42 +0530

    Add prune_default module

---
+++ src/prune_default.pm
@@ -0,0 +1,38 @@
+package prune_default;
+use feature_filter;
+use shakti_tree_api;
+use Exporter qw(import);
+
+our @EXPORT = qw(prune_default);
+
+sub prune_default{
+
+
+	# Keep the first FS in the case of more than one
+	$sent=@_[0];
+	my($parent);
+	my($fs,@attr,@index);
+	@index = &get_leaves($sent);
+	
+	for($i=0 ; $i<=$#index ; $i++)
+	{
+		($f0,$f1,$f2,$f3,$f4) = &get_fields($index[$i],$sent);
+		$fs_ptr = &read_FS($f4,$sent);
+
+		#first, update all the category in the FS based on the POS
+
+		$num_of_fs = &get_num_fs($fs_ptr,$sent);
+		if($num_of_fs > 1)
+		{
+			for($j = 2; $j <= $num_of_fs; $j++)
+			{
+				$ret=&prune_FS("",$j,$fs_ptr,$sent);
+			}
+		}
+
+		$string=&make_string($fs_ptr,$sent);
+		&modify_field($index[$i],4,$string,$sent);
+	}
+}
+
+1;
